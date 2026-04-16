929b9a8f7a827c4ce56e854de38e8aa906c55d5c
Ron Israeli <ronisra100@gmail.com>
2026-04-16 21:23:41 +0300

    cr: change config method

--- src/common/syncConfig.ts
+++ src/common/syncConfig.ts
@@ -1,12 +1,7 @@
 import type { SyncConfig } from '../types';
+import { getConfig } from './config';
 
 // TODO: replace with DB-backed config from syncConfigRepository - then delete this file
 export function getSyncConfig(): SyncConfig {
-  return {
-    layers: (process.env.SYNC_LAYERS ?? 'obstacles').split(','),
-    syncIntervalMs: Number(process.env.SYNC_INTERVAL_MS) || 500,
-    pollIntervalMs: Number(process.env.SYNC_POLL_INTERVAL_MS) || 600_000,
-    pageSize: Number(process.env.SYNC_PAGE_SIZE) || 500,
-    thirdPartyBaseUrl: process.env.THIRD_PARTY_BASE_URL ?? 'http://mock-third-party/graphql',
-  };
+  return getConfig().get('sync') as SyncConfig;
 }
